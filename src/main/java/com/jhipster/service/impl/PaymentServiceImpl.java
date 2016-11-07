3b2f9fc3614afb6979a5141a0e2b50b8a9e60943
arunas <arunas.stonis@gmail.com>
2016-11-07 22:23:32 +0200

    added audit publisher for Stripe payment integration calls

--- src/main/java/com/jhipster/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/jhipster/service/impl/PaymentServiceImpl.java
@@ -7,22 +7,34 @@ import com.jhipster.service.dto.PaymentDTO;
 import com.jhipster.service.mapper.PaymentMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.stripe.net.RequestOptions;
+import com.stripe.model.Charge;
+import com.stripe.exception.StripeException;
+
+import com.jhipster.security.AuditEventPublisher;
 
 /**
  * Service Implementation for managing Payment.
  */
 @Service
 public class PaymentServiceImpl implements PaymentService{
-
+ 	
     private final Logger log = LoggerFactory.getLogger(PaymentServiceImpl.class);
+
+    @Inject
+    private AuditEventPublisher auditPublisher;   
     
     @Inject
     private PaymentRepository paymentRepository;
@@ -37,6 +49,20 @@ public class PaymentServiceImpl implements PaymentService{
      * @return the persisted entity
      */
     public PaymentDTO save(PaymentDTO paymentDTO) {
+	
+    	RequestOptions requestOptions = RequestOptions.builder().setApiKey("sk_test_Mxfnl3XntyJuddbT5t9DrkBq").build();
+        try {
+            Charge charge = Charge.retrieve("ch_19Bka6KnHgK6bLInmGlxPgH5", requestOptions);
+            log.info("Found a payment from Stripe: {}", charge.getAmount()/100);    
+//        	String principal = tokenProvider.getUserNameFromToken(authToken);
+        	AuditEvent event = new AuditEvent("SYSTEM", "STRIPE CALL"+charge.getAmount(), new HashMap<String, Object>());
+        	auditPublisher.publish(event);
+        } catch (StripeException e) {
+            e.printStackTrace();
+        	AuditEvent event = new AuditEvent("SYSTEM", "STRIPE CALL"+e.getMessage(), new HashMap<String, Object>());
+        	auditPublisher.publish(event);
+        }
+    	
         log.debug("Request to save Payment : {}", paymentDTO);
         Payment payment = paymentMapper.paymentDTOToPayment(paymentDTO);
         payment = paymentRepository.save(payment);
